9a36576c144a10b627af845d01fd42d035cee5ee
Igor Kulman <igor@kulman.sk>
2013-12-28 13:28:35 +0100

    + init commit

---
+++ WebConsole/Controllers/ConsoleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebConsole.Controllers
+{
+    public class ConsoleController : Controller
+    {
+        //
+        // GET: /Console/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
